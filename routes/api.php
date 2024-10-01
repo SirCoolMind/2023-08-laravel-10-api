0f64f95008d468fbe771aa91fa75ae59b63f64b3
Hafiz Unijaya <hafizcoolman@gmail.com>
2024-10-01 08:17:47 +0800

    [API] temp disable register route

--- routes/api.php
+++ routes/api.php
@@ -23,7 +23,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 //Public route
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/register', [AuthController::class, 'register']);
+// Route::post('/register', [AuthController::class, 'register']);
 
 //Protected route
 Route::group(['middleware' => ['auth:sanctum']], function(){
