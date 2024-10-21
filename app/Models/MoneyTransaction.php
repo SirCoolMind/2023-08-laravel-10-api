794f155add4e8b8d46c77dda282c659d8b8b18bf
Hafiz Unijaya <hafizcoolman@gmail.com>
2024-10-21 08:34:54 +0800

    [Finance] Add base module

---
+++ app/Models/MoneyTransaction.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MoneyTransaction extends Model
+{
+    use HasFactory;
+
+
+    /**
+     * LIST OF CATEGORY ENUM THINGY
+     *
+     * UTILITIES
+     * FOOD
+     * ENTERTAINMENT
+     * TRANSPORTATION
+     * FAMILY
+     *
+     *
+     *
+     *
+     */
+}
