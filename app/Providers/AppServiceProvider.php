816ae523741076d1c84bcb2ef7d3d386e5c396e4
hafiz <hafizcoolman@gmail.com>
2024-11-11 16:02:25 +0800

    [Composer] Add API Documentation

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Dedoc Scramble Setup
+        \Dedoc\Scramble\Scramble::afterOpenApiGenerated(function (\Dedoc\Scramble\Support\Generator\OpenApi $openApi) {
+            $openApi->secure(
+                \Dedoc\Scramble\Support\Generator\SecurityScheme::http('bearer')
+            );
+        });
+
+        \Illuminate\Support\Facades\Gate::define('viewApiDocs', function (\App\Models\User $user) {
+            // Gate viewApiDocs is for environment other than local
+            return in_array($user?->email, ['hafizcoolman@gmail.com']);
+        });
+        // END Dedoc Scramble Setup
     }
 }
