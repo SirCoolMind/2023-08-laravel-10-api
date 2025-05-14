f448c571198b8a6e8204b2ab10925283d164c610
Hafiz Ruslan <hafizcoolman@gmail.com>
2025-05-14 18:59:11 +0800

    [Finance] Add middleware to lookup categories, subcategories

--- packages/finance/src/routes/lookup.php
+++ packages/finance/src/routes/lookup.php
@@ -8,12 +8,12 @@ Route::group(['prefix' => 'lookup'], function () {
     Route::group(['middleware' => ['auth:api']], function () {
         Route::get('clear-categories-cache', [LookupController::class, 'clearCategoriesCache']);
         Route::get('get-accounts', [LookupController::class, 'getAccounts']);
+        Route::get('get-categories', [LookupController::class, 'getCategories']);
+        Route::get('get-sub-categories', [LookupController::class, 'getSubCategories']);
     });
 
     // Public route
     Route::get('get-categories-enum', [LookupController::class, 'getCategoriesEnum']);
     Route::get('get-sub-categories-enum', [LookupController::class, 'getSubCategoriesEnum']);
-    Route::get('get-categories', [LookupController::class, 'getCategories']);
-    Route::get('get-sub-categories', [LookupController::class, 'getSubCategories']);
     Route::get('get-finance-type-enum', [LookupController::class, 'getFinanceTypeEnums']);
 });
