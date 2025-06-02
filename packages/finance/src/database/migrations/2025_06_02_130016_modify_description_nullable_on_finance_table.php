4c7e4583fbbc92a4d4922b8f45f30a214d8274b9
Hafiz Ruslan <hafizcoolman@gmail.com>
2025-06-02 15:51:00 +0800

    [Finance] Change description column to nullable

---
+++ packages/finance/src/database/migrations/2025_06_02_130016_modify_description_nullable_on_finance_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::table('money_accounts', function (Blueprint $table) {
+            $table->text('description')->nullable()->change();
+        });
+        Schema::table('money_categories', function (Blueprint $table) {
+            $table->text('description')->nullable()->change();
+        });
+        Schema::table('money_subcategories', function (Blueprint $table) {
+            $table->text('description')->nullable()->change();
+        });
+    }
+
+    public function down(): void
+    {
+        // Set all NULL description values to an empty string (or any default)
+        DB::table('money_accounts')
+            ->whereNull('description')
+            ->update(['description' => '']);
+        DB::table('money_categories')
+            ->whereNull('description')
+            ->update(['description' => '']);
+        DB::table('money_subcategories')
+            ->whereNull('description')
+            ->update(['description' => '']);
+
+        Schema::table('money_accounts', function (Blueprint $table) {
+            $table->text('description')->nullable(false)->change();
+        });
+        Schema::table('money_categories', function (Blueprint $table) {
+            $table->text('description')->nullable(false)->change();
+        });
+        Schema::table('money_subcategories', function (Blueprint $table) {
+            $table->text('description')->nullable(false)->change();
+        });
+    }
+};
