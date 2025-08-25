d2bf5b359f4af597c7d90a16804593167f1de4a2
Hafiz Ruslan <hafizcoolman@gmail.com>
2025-08-26 05:21:31 +0800

    [Finance] Add deleted_at for money transaction

--- packages/finance/src/app/Models/MoneyTransaction.php
+++ packages/finance/src/app/Models/MoneyTransaction.php
@@ -5,11 +5,13 @@ namespace HafizRuslan\Finance\app\Models;
 use HafizRuslan\Finance\app\Enums\FinanceTypeEnum;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use SirCoolMind\UploadedFiles\app\Models\UploadedFile;
 
 class MoneyTransaction extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     const FileTypeTransactionImages = 'transaction_images';
 
